c5d029545d8c846ebf00a5189ebf36a3c7cd2635
ivanfebrianto <specopskit@gmail.com>
2023-01-08 22:26:01 +0700

    tes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -29,6 +29,7 @@
                             href="{{ route("landing") }}">Home</a></li>
                     <li class="nav-item"><a class="nav-link" href="#contact">Contact</a></li>
                     <li class="nav-item"><a class="nav-link" href="#features">features</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#aboutus">About Us</a></li>
                     <li class="nav-item"><a class="nav-link" href="{{ route('login') }}">Login</a></li>
                 </ul>
             </div>
